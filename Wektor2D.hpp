7d6c3c13cfb5554b304aff232a25e80e02aed192
Michał Tomporowski <michalang400@gmail.com>
2025-10-22 05:39:38 +0200

    Wykonano pracę na zejściówkę

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,32 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+	Wektor2D()
+	{
+		v1 = 0;
+		v2 = 0;
+	}
+	Wektor2D(double x, double y)
+	{
+		v1 = x;
+		v2 = y;
+	}
+	void setX(double variable) { v1 = variable; }
+	void setY(double variable) { v2 = variable; }
+	double getX() { return v1; }
+	double getY() { return v2; }
+
+private:
+	double v1;
+	double v2;
 };
+
+Wektor2D operator+(Wektor2D sk³adnik1, Wektor2D sk³adnik2)
+{
+	return Wektor2D{ sk³adnik1.getX() + sk³adnik2.getX(), sk³adnik1.getY() + sk³adnik2.getY()};
+}
+
+Wektor2D operator*(Wektor2D sk³adnik1, Wektor2D sk³adnik2)
+{
+	return Wektor2D{ sk³adnik1.getX() * sk³adnik2.getX(), sk³adnik1.getY() * sk³adnik2.getY() };
+}
